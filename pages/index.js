602bcacecf5b85c3e81dd5f9f8ab0fac050010a4
petsieiq <admin@petsieiq.com>
2025-04-02 05:42:03 +0100

    Update index.js
    Co-Authored-By: petsieiq <205852848+petsieiq@users.noreply.github.com>

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 import { builder, BuilderComponent } from '@builder.io/react';
 
-builder.init(381e5855759f41dcb2768e737d1919d7); // Your actual public API key
+builder.init('381e5855759f41dcb2768e737d1919d7'); // Your actual public API key
 
 export default function Home() {
   return <BuilderComponent model="page" />;
