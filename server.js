e2665b86d495a03601ac29d5664d653ef4e1d12d
deepshankarjha <164512511+deepshankarjha@users.noreply.github.com>
2025-04-03 01:36:12 +0530

    update fix for cold start

--- server.js
+++ server.js
@@ -4,7 +4,29 @@ const express = require("express");
 var app = express();
 const path = require("path");
 const hbs = require('hbs');
-const db_api = require("./db_api");
+
+var mongo_client = require("mongodb").MongoClient;
+
+class DatabaseAPI{
+    constructor(){
+
+    }
+
+    //databse setup
+    async init() {
+        //let url = "mongodb://localhost:27017/";
+        let url = "mongodb+srv://admin:ey3Q76SUjTgxnjBc@petsieiqdb.07dy5vz.mongodb.net/?retryWrites=true&w=majority&appName=PetsieIQDB";
+        let db_conn =await mongo_client.connect(url);
+        this.db_obj = await db_conn.db("email_db");
+        this.email_collection =await this.db_obj.createCollection("emails");
+    }
+
+    async add_email(data){
+        let reps =await this.email_collection.insertOne(data);
+    }
+}
+
+module.exports = {DatabaseAPI:DatabaseAPI};
 
 db_obj = new db_api.DatabaseAPI();
 db_obj.init();
@@ -16,14 +38,11 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.urlencoded());
 app.use(express.json());
 
-app.get('/', (req, res) => {
-    const minm = 100000;
-    const maxm = 999999;
-    const number = Math.floor(Math.random() * (maxm - minm + 1)) + minm;
+app.get('/', async (req, res) => {
     res.render('index', {});
 });
 
-app.post('/', (req, res) => {
+app.post('/',async (req, res) => {
     let email = req.body.email_input;
     //console.log("the pose request received:" + email);
     //update the data base
